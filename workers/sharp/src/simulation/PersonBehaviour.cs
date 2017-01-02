0f97af9ae7285fdb6501cd65efb85bc9a57f95d2
Harry Ragan <harry.ragan@gmail.com>
2017-01-02 14:23:54 +0000

    Implement basic movement

--- workers/sharp/src/simulation/PersonBehaviour.cs
+++ workers/sharp/src/simulation/PersonBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Improbable;
+using Improbable.Math;
 using Improbable.Worker;
 using Ragan;
 using SharpWorker.framework;
@@ -11,13 +12,21 @@ namespace SharpWorker.simulation
   {
     private readonly SerializedConnection _conn;
     private readonly EntityId _entityId;
+
+    private Coordinates _currentPosition;
     private int _currentWealth;
+    private Coordinates _target;
 
     public PersonBehaviour(SerializedConnection conn, IComponentData<Person> value, EntityId entityId)
     {
       _conn = conn;
       _entityId = entityId;
-      _currentWealth = value.Get().Value.wealth.current;
+
+      var data = value.Get().Value;
+      _currentWealth = data.wealth.current;
+      _currentPosition = data.position;
+
+      _target = new Coordinates(100, 100, 100);
     }
 
     public void AuthorityChanged(bool hasAuthority)
@@ -25,11 +34,55 @@ namespace SharpWorker.simulation
       if (hasAuthority)
       {
         Task.Run(() => StartUpdatingWealth());
+        Task.Run(() => MoveToTarget());
       }
       else
       {
         //TODO(harry) - stop the task here.
       }
+    }
+
+    public void Update(IComponentUpdate<Person> componentUpdate)
+    {
+      var wealthOption = componentUpdate.Get().wealth;
+      if (wealthOption.HasValue)
+      {
+        _currentWealth = wealthOption.Value.current;
+      }
+
+      var positionOption = componentUpdate.Get().position;
+      if (positionOption.HasValue)
+      {
+        _currentPosition = positionOption.Value;
+      }
+    }
+
+    private void MoveToTarget()
+    {
+      int steps = 100;
+      var originalPosition = _currentPosition;
+      for (int i = 1; i < steps; i ++)
+      {
+        var newPosition = new Coordinates{
+          X = Lerp(originalPosition.X, _target.X, i, steps),
+          Y = Lerp(originalPosition.Y, _target.Y, i, steps),
+          Z = Lerp(originalPosition.Z, _target.Z, i, steps),
+        };
+
+        var componentUpdate = new Person.Update
+        {
+          position = newPosition
+        };
+
+        _conn.Do(c => c.SendComponentUpdate(_entityId, componentUpdate));
+
+        Thread.Sleep(1000);
+      }
+    }
+
+    private double Lerp(double original, double target, int currentStep, int steps)
+    {
+      return original + ((target - original) * (double)currentStep / steps);
     }
 
     private void StartUpdatingWealth()
@@ -47,15 +100,6 @@ namespace SharpWorker.simulation
         _conn.Do(c => c.SendComponentUpdate(_entityId, componentUpdate));
 
         Thread.Sleep(1000);
-      }
-    }
-
-    public void Update(IComponentUpdate<Person> componentUpdate)
-    {
-      var wealthOption = componentUpdate.Get().wealth;
-      if (wealthOption.HasValue)
-      {
-        _currentWealth = wealthOption.Value.current;
       }
     }
   }
